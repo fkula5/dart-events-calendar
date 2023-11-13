0e7c00b46f1a5aa58d6bcaf581910ab9d7ea7d53
Tucznik2 <f3kula@gmail.com>
2023-11-13 02:00:36 +0100

    feat: model factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         \App\Models\Venue::factory(10)->create();
+         \App\Models\Event::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
